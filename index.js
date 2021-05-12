9386f33939394c9924e5058c9444f03c77bcdaa3
Ryan Erricson <ryanerricson@gmail.com>
2021-05-12 12:44:01 -0400

    commit tree 9abe19ff0d03dbb4337bc101075d0925e5735ae4

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+function findMatching(arr, str){
+   return arr.filter(n => {
+        return (n.toLowerCase() === str.toLowerCase())
+    })
+
+}
+
+function fuzzyMatch(arr, s){
+    return arr.filter(n => {
+         return (n[0] === s[0])
+     })
+ }
+
+ function matchName(arr, s){
+     return arr.filter(n =>{
+        return n.name === s
+     })
+ }
+
